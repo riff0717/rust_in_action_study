a6b420c0abe9fbca077430dc7cd51be657edcc9f
riff0717 <riffriff717@gmail.com>
2022-05-16 14:41:07 +0000

    p9まで反映

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -1,3 +1,13 @@
+fn greet_world(){
+    println!("Hello world!");
+    let southern_germany = "GrǔβGott！";
+    let japan = "ハロー・ワールド";
+    let regions = [southern_germany, japan];
+    for region in regions.iter() {
+        println!("{}", &region);
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    greet_world();
 }
