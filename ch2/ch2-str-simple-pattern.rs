fc768af16c54fd1a1192b60a581e4759838dcbd9
riff0717 <riffriff717@gmail.com>
2022-09-25 04:01:59 +0000

    p71,grep-liteまで

---
+++ ch2/ch2-str-simple-pattern.rs
@@ -0,0 +1,13 @@
+fn main(){
+    let search_term = "picture";
+    let quote = "\
+Every face, every shop bedroom window, publlic-house, and
+dark square is picture feverishly turnes--in search of what?
+It is the same with books.
+What do we seek through millions of pages?";
+    for line in quote.lines() {
+        if line.contains(search_term) {
+            println!("{}", line);
+        }
+    }
+}
