3fd740fdb0af55e4fb8a5a05fc80ad8da0772be7
riff0717 <riffriff717@gmail.com>
2022-09-27 14:08:30 +0000

    p83,list2-26まで

--- ch2/grep-lite/src/main.rs
+++ ch2/grep-lite/src/main.rs
@@ -1,7 +1,18 @@
 use regex::Regex;
+use clap::{App,Arg};
 
 fn main(){
-    let re = Regex::new("picture").unwrap();
+    let args = App::new("grep-lite")
+        .version("0.1")
+        .about("serches for patterns")
+        .arg(Arg::with_name("pattern")
+            .help("The pattern to search for")
+            .takes_value(true)
+            .required(true))
+        .get_matches();
+    
+    let pattern = args.value_of("pattern").unwrap();
+    let re = Regex::new(pattern).unwrap();
     
     let quote = "Every face, every shop bedroom window, publlic-house, and
 dark square is picture feverishly turnes--in search of what?
